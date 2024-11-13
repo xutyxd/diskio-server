461d9bc0e19844882840bf29e7cb9de32fc021b2
Xuty <xutyxd@gmail.com>
2024-11-14 00:01:08 +0100

    fix(test): fixed creation of healthCheckController on test

--- tests/units/health-check/controllers/health-check.controller.spec.ts
+++ tests/units/health-check/controllers/health-check.controller.spec.ts
@@ -7,6 +7,8 @@ import { HealthCheckController } from "../../../../src/server/crosscutting/healt
 import { IHealthCheckModelData } from "../../../../src/server/crosscutting/health-check/interfaces/data";
 import { HealthCheckRepository } from "../../../../src/server/crosscutting/health-check/repository/health-check.repository";
 import { HealthCheckService } from "../../../../src/server/crosscutting/health-check/services/health-check.service";
+import { DiskioService } from "../../../../src/server/diskio/services/diskio.service";
+import { ConfigurationService } from "../../../../src/server/configuration/services/configuration.service";
 
 describe('HealthCheckController', () => {
 
@@ -18,7 +20,9 @@ describe('HealthCheckController', () => {
                 const database = new MemoryDatabaseService<IHealthCheckModelData>();
                 database.connection.open();
                 const repository = new HealthCheckRepository(database);
-                const service = new HealthCheckService(repository);
+                const configurationService = new ConfigurationService();
+                const diskioService = new DiskioService(configurationService);
+                const service = new HealthCheckService(repository, diskioService);
 
                 instance = new HealthCheckController(service);
             } catch (e) {
